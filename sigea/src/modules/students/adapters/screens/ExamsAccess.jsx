7ab2a368782cb686dd46c1ca191cf549eff27166
AnettVera <20223tn034@utez.edu.mx>
2024-02-25 09:35:38 -0600

    Condicion para acceder a exam

--- sigea/src/modules/students/adapters/screens/ExamsAccess.jsx
+++ sigea/src/modules/students/adapters/screens/ExamsAccess.jsx
@@ -1,8 +1,13 @@
 import { StyleSheet, Image, Text, View, TouchableOpacity, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Welcome from '../../../../../assets/img/welcome.png'
+import Exam from './Exam'
+import { useNavigation } from '@react-navigation/native';
 
 export default function ExamsAccess() {
+    const navigation = useNavigation();
+    const [accessCode, setAccessCode] = useState('');
+
     const user =
     {
         name: 'Marbein Getsemani',
@@ -14,29 +19,30 @@ export default function ExamsAccess() {
     return (
         <View style={styles.container}>
             <View style={styles.card}>
-                <View style={{alignItems:'center'}}>
-                <Image source={Welcome} style={styles.img} />
-                <Text style={styles.title}>{`¡Hola ${user.name}!`}</Text>
+                <View style={{ alignItems: 'center' }}>
+                    <Image source={Welcome} style={styles.img} />
+                    <Text style={styles.title}>{`¡Hola ${user.name}!`}</Text>
                 </View>
-                
+
 
                 <View style={styles.input}>
-                   
+
                     <TextInput
                         placeholder='Codigo de acceso'
                         placeholderTextColor='#6b7288'
                         style={styles.inputControl}
+                        value={accessCode}
+                        onChangeText={(text) => setAccessCode(text)}
                     />
                 </View>
 
                 <View style={styles.formAction}>
-                    <TouchableOpacity
-
-                    >
+                    <TouchableOpacity onPress={() => accessCode.trim() !== '' && navigation.navigate('Exam')}>
                         <View style={styles.btn}>
                             <Text style={styles.btnText}>Acceder</Text>
                         </View>
                     </TouchableOpacity>
+
                 </View>
             </View>
         </View>
@@ -51,9 +57,14 @@ const styles = StyleSheet.create({
         alignContent: 'center',
         paddingHorizontal: 15,
     }, card: {
-        backgroundColor: '#fff',
+        backgroundColor: '#E4EAF8',
         borderRadius: 15,
         padding: 10,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
     }, title: {
         fontSize: 18,
         fontWeight: '800',
@@ -61,8 +72,8 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     }, input: {
         marginBottom: 10,
-        alignItems:'left',
-        textAlign:'left'
+        alignItems: 'left',
+        textAlign: 'left'
     },
     inputControl: {
         borderWidth: 2,
@@ -74,24 +85,25 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontWeight: '500',
         color: '#222',
-        marginTop:30,
+        marginTop: 30,
     },
     btn: {
         backgroundColor: '#4480FF',
         borderRadius: 8,
         borderWidth: 1,
         height: 40,
+        width: '80%',
         borderColor: '#4480FF',
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center',
         marginTop: 10,
-        paddingHorizontal: 30,
+        paddingHorizontal: 100,
     },
     btnText: {
         fontSize: 18,
         fontWeight: '600',
         color: '#fff',
+        textAlign: 'center'
     },
     form: {
         marginBottom: 20,
@@ -99,6 +111,7 @@ const styles = StyleSheet.create({
     },
     formAction: {
         marginVertical: 10,
+        alignItems: 'center',
     }, img: {
         width: 150,
         height: 150,
