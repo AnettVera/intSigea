7ac27b2903c94a85e72771c57f26e7e0d95f23cd
Nieobe Hernandez <20223tn057@utez.edu.mx>
2024-03-26 22:07:04 -0600

    para visualizar los resultados del examen

--- sigea/src/modules/students/adapters/screens/Results.jsx
+++ sigea/src/modules/students/adapters/screens/Results.jsx
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Results() {
+export default function Results(props) {
+    const { params } = props.route;
   return (
-    <View>
-      <Text>Results</Text>
+    <View style={styles.container}>
+         <Text style={{ fontWeight: "bold", color:"#052368", fontSize:'16' }}>{params.title}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        padding: 32,
+      },
+})
